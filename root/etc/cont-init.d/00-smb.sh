425a5eb9419e970c81eb4404ff169b3f67114e4f
Rick Box <boxrick@gmail.com>
2023-12-30 21:48:18 +0000

    fix: Add fix for "groupadd: group time-machine-users already exists", this occurs when you restart a container without destroying and re-creating

--- root/etc/cont-init.d/00-smb.sh
+++ root/etc/cont-init.d/00-smb.sh
@@ -3,14 +3,12 @@
 set -eu
 set -o pipefail
 
-
 GROUP_NAME='time-machine-users'
 
-groupadd --non-unique --gid "$PGID" "$GROUP_NAME"
-useradd --system --non-unique --gid "$GROUP_NAME" --uid "$PUID" "$SMB_USER"
-
+# Check if group or username exists, if they do skip over
+getent group "$GROUP_NAME" || groupadd --non-unique --gid "$PGID" "$GROUP_NAME"
+id -u "$SMB_USER" &>/dev/null || useradd --system --non-unique --gid "$GROUP_NAME" --uid "$PUID" "$SMB_USER"
 
 printf '%s\n%s\n' "$SMB_PASSWORD" "$SMB_PASSWORD" | smbpasswd -s -a "$SMB_USER"
-
 
 chown "$SMB_USER":"$GROUP_NAME" '/share'
